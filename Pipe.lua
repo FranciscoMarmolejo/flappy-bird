b7a5992736dc93f9e19347d4c75b2910dacba307
Garret Ravelli De La Cruz Tadeo <garrettravelly@gmail.com>
2021-02-24 12:24:42 -0600

    Pipe 2.0 --> recorte sin salirse del area del rectangulo.

--- Pipe.lua
+++ Pipe.lua
@@ -1,5 +1,7 @@
 Pipe = Class{}
 local PIPE_IMAGE = love.graphics.newImage("img/pipe.png")
+local ancho = PIPE_IMAGE:getWidth()*.15
+local alto = PIPE_IMAGE:getHeight()*.15
 function Pipe:init(x,y,w,h,speed,r)
 	self.x = x
 	self.y = y
@@ -15,12 +17,15 @@ function Pipe:update(dt)
 end
 
 function Pipe:render()
+	PIPE_SPRITE= love.graphics.newQuad(0,0,self.w, self.h,ancho,alto)
+
 	love.graphics.setColor(.5, 1, 0)
-	--love.graphics.rectangle('line',self.x, self.y, self.w, self.h)
+	love.graphics.rectangle('line',self.x, self.y, self.w, self.h)
 	if self.rotation == false then
-		love.graphics.draw(PIPE_IMAGE,self.x,self.y,0,.145,.15)
+		--love.graphics.draw(PIPE_IMAGE,self.x,self.y,0,.145,.15) <--Metodo anterior
+		love.graphics.draw(PIPE_IMAGE,PIPE_SPRITE,self.x,self.y)
 	elseif self.rotation == true then
-		love.graphics.draw(PIPE_IMAGE,self.x+self.w,self.y+self.h,math.pi,.145,.15)
+		love.graphics.draw(PIPE_IMAGE,PIPE_SPRITE,self.x+self.w,self.y+self.h,math.pi)
 	end	
 
 end
